90407b17ca3506f04b0a164ed3158f55b876f07b
Kajal <rina.skyler@yahoo.com>
2026-09-03 20:45:16 +0300

    kajal adding template for login

---
+++ login.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def login():
+    username = username.entry.get()
+    password = password.entry.get()
+    if username == "admin" and password =="password":
+        messagebox.showinfo("Login", "Login successful")
+    else:
+        messagebox.showerror("Login", "invalid username or password")
+
+
+root = tk.Tk()
+root.title("Login")
